d2dff7eaaf96c14b4042d19f14017e8ec23a3f9f
Nishant Kumar Gupta <nishantkumargupta728@gmail.com>
2024-11-19 00:22:29 +0530

    update emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,9 +30,9 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
         from_name:form.name,
-        to_name:'Rohan',
+        to_name:'Nishant',
         from_email:form.email,
-        to_email:'rohan7690kt@gmail.com',
+        to_email:'nishantkumargupta@gmail.com',
         message:form.message,
       },import.meta.env.VITE_APP_EMAILJS_USER_ID)
       .then(()=>{
